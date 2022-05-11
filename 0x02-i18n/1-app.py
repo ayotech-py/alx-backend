4049eddaea829224b228d2ce68b2c982e754aa88
Emediong Francis <emediongfrancis@gmail.com>
2022-05-11 16:26:25 +0100

    Initialize Babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -16,7 +16,7 @@ class Config(object):
     '''
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TZ = 'UTC'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
